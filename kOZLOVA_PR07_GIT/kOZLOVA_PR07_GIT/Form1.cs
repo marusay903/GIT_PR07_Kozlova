bdf13e67f73cbc178020968c5b56394087e45ad6
Kozlova Maria <Kozlova_Maria903@gmail.com>
2025-11-30 13:06:14 +0300

    Реазилован преход между формами

--- kOZLOVA_PR07_GIT/kOZLOVA_PR07_GIT/Form1.cs
+++ kOZLOVA_PR07_GIT/kOZLOVA_PR07_GIT/Form1.cs
@@ -16,5 +16,11 @@ namespace kOZLOVA_PR07_GIT
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form2 adminForm = new Form2();
+            adminForm.Show();
+        }
     }
 }
